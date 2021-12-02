0d82c202bcfb72b4e931cee85f071ac1d60fe9a2
Shams Arefin Khan <robeeen@gmail.com>
2021-12-02 23:03:49 +0600

    First commit.
    Loaded @chakra-ui/react @emotion/react @emotion/styled axios framer-motion millify nprogress react-horizontal-scrolling-menu react-icons

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import { Text, Box, Flex, Button} from '@chakra-ui/react'
+
+
+export default function Home() {
+  return (
+    <div >
+      
+    </div>
+  )
+}
